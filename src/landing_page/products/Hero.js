228417da4a058983f5f9274ce084ca5da222578a
pmkastu25 <pmkastu25@gmail.com>
2025-02-14 13:40:48 +0530

    products-hero part completed

--- src/landing_page/products/Hero.js
+++ src/landing_page/products/Hero.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>
-            Hero
-        </h1>
+        <div className="container">
+            <div className="text-center mt-5 p-3">
+            <h1>Zerodha Products</h1>
+            <h3 className="text-muted mt-3 fs-4">Sleek, modern, and intuitive trading platforms</h3>
+            <p className="mt-3">Check out our <a href="" className="flinks">investment offerings <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
+            </div>
+        </div>
      );
 }
 
