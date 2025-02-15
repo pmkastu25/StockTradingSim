630ad6eb7fa7f8a23e2ede63b45836c24e2a3e71
pmkastu25 <pmkastu25@gmail.com>
2025-02-15 13:27:05 +0530

    support Component completed

--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -2,9 +2,31 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>
-            Hero
-        </h1>
+        <section className='container-fluid' id="supportHero" >
+            <div className=''></div>
+            <div className='pt-5 ' id="supportWrapper">
+                <h4>
+               Support Portal
+                </h4>
+                <a href="">Track Tickets</a>
+            </div>
+            <div className='row p-3 m-3 outer'>
+            <div className='col-6 p-3 px-4 inner'>
+                <h4  className='fs-3 mb-5'>Search for an answer or browse help topics to create a ticket</h4>
+                <input placeholder='Eg. how do I activate F&O, why is my order getting rejected..'/><br/>
+                <a href="">Track account opening</a> 
+                <a href="">Track segment activation</a> <a href="">Intraday margins </a>
+                <a href="">Kite user manual</a>
+            </div>
+            <div className='col-6 p-3 px-4 inner'>
+            <h1 className='fs-3'>Featured</h1>
+            <ol>
+                <li><a href="">MCX Crude option contract expiry - February 2025</a> </li>
+                <li><a href="">Latest Intraday leverages and Square-off timings</a></li>
+            </ol>
+            </div>
+            </div>
+        </section>
      );
 }
 
