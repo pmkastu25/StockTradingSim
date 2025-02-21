0d2940605853cb7f21f04a3e3a89752af84738c7
pmkastu25 <pmkastu25@gmail.com>
2025-02-22 00:39:10 +0530

    Added Chart.js functionality

--- src/components/BuyActionWindow.js
+++ src/components/BuyActionWindow.js
@@ -1,33 +1,49 @@
-import React from "react";
+import React,{useState }from "react";
 
 import { Link } from "react-router-dom";
 import "./BuyActionWindow.css";
 
+import axios from "axios"
+
 import GeneralContext from "./GeneralContext";
 
 const BuyActionWindow = ({ uid }) => {
+  const [stockQuantity, setstockQuantity] = useState(1);
+  const [stockPrice, setstockPrice] = useState(0.0);
+
+  const handleBuyClick = () => {
+    axios.post("http://localhost:3005/newOrder", {
+      name: uid,
+        qty: stockQuantity,
+        price: stockPrice,
+        mode: "BUY",
+    })
+
+    GeneralContext.closeBuyWindow();
+  }
+
   const handleCancelClick = () => {
     GeneralContext.closeBuyWindow();
   };
 
   return (
-    <div className="containerClass" id="buy-window" draggable="true">
+    <div className="container" id="buy-window" draggable="true">
       <div className="regular-order">
         <div className="inputs">
           <fieldset>
             <legend>Qty.</legend>
-            <input type="number" name="qty" id="qty" />
+            <input type="number" name="qty" id="qty" onChange={(e)=> setstockQuantity(e.target.value)} value={stockQuantity}/>
           </fieldset>
           <fieldset>
             <legend>Price</legend>
-            <input type="number" name="price" id="price" step="0.05" />
+            <input type="number" name="price" id="price" step="0.05" onChange={(e)=> setstockPrice(e.target.value)} value={stockPrice}/>
           </fieldset>
         </div>
       </div>
       <div className="buttons">
         <span>Margin required &#x20B9;140.65</span>
         <div>
-          <Link className="btn btn-blue">Buy</Link>
+          <Link className="btn btn-blue" onClick={handleBuyClick}>Buy</Link>
           <Link
             to=""
             className="btn btn-grey"
