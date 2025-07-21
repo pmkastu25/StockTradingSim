374f59bb1f174a043bc8a6f84c2cbe05535075b1
pmkastu25 <pmkastu25@gmail.com>
2025-07-21 20:04:03 +0530

    moved all files to frontend folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import{ BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 
-import HomePage from './landing_page/home/HomePage';
-import Signup from './landing_page/signup/Signup';
-import AboutPage from './landing_page/about/AboutPage';
-import ProductPage from './landing_page/products/ProductPage';
-import PricingPage from './landing_page/pricing/PricingPage';
-import SupportPage from './landing_page/support/SupportPage';
-import Navbar from './landing_page/Navbar';
-import Footer from './landing_page/Footer';
-import NotFound from './landing_page/NotFound';
+import HomePage from './landing_page/home/HomePage.js';
+import Signup from './landing_page/signup/Signup.js';
+import AboutPage from './landing_page/about/AboutPage.js';
+import ProductPage from './landing_page/products/ProductPage.js';
+import PricingPage from './landing_page/pricing/PricingPage.js';
+import SupportPage from './landing_page/support/SupportPage.js';
+import Navbar from './landing_page/Navbar.js';
+import Footer from './landing_page/Footer.js';
+import NotFound from './landing_page/NotFound.js';
 import Login from './landing_page/Login/Login.js';
-import { AuthProvider } from "./context/AuthContext"; // adjust path
+import { AuthProvider } from "./context/AuthContext.js"; // adjust path
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
